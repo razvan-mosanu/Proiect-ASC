27f22222623e4d40d9d98f62d123adafdb46d4c6
razvan-mosanu <razvanamosanu@gmail.com>
2026-01-30 11:52:44 +0200

    Update input.s

--- test/Lab 0x04/Ex 0x02 Lab 0x04/input.s
+++ test/Lab 0x04/Ex 0x02 Lab 0x04/input.s
@@ -1,1 +1,32 @@
+.data
+n: .long 5234
+s: .long 0
+formatAfSuma: .ascii "Suma cifrelor numarului este: %ld\n"
+text
+.global main
+main:
+    movl n, %ecx
+    movl $10, %ebx
+    et_loop:
+    cmp $0, %ecx
+    je et_afisare
+    movl %ecx, %eax
+    xorl %edx, %edx
+    divl %ebx
+    addl %edx, s
+    movl %eax, %ecx
+    jmp et_loop
+et_afisare:
+    pushl s
+    push $formatAfSuma
+    call printf
+    popl %ebx
+    popl %ebx
+    pushl stdout
+    call fflush
+    addl $4, %esp
+et_exit:
+    movl $1, %eax
+    xorl %ebx, %ebx
+    int $0x80
 

7acedb1cc12244a341d6b01f419eecbee1f7ebe1
razvan-mosanu <razvanamosanu@gmail.com>
2026-01-30 11:52:25 +0200

    Update input.s

--- test/Lab 0x04/Ex 0x01 Lab 0x04/input.s
+++ test/Lab 0x04/Ex 0x01 Lab 0x04/input.s
@@ -1,1 +1,40 @@
+.data
+n: .long 9
+v: .long 12, 15, 5, 15, 4, 1, 7, 15, 1
+maxim: .long 0
+ap: .long 0
+.text
+.global main
+main:
+    movl n, %ecx
+    movl $v, %edi
+et_loop:
+    movl n, %ebx
+    subl %ecx, %ebx
+    movl (%edi, %ebx, 4), %edx
+    cmp maxim, %edx
+    jg et_max_nou
+    loop et_loop
+    jmp cont_max
+et_max_nou:
+    movl %edx, maxim
+    loop et_loop
+    cont_max:
+    movl n, %ecx
+    movl $v, %edi
+    loop_ap:
+    movl n, %ebx
+    subl %ecx, %ebx
+    movl (%edi, %ebx, 4), %edx
+    cmp maxim, %edx
+    je et_egale
+    loop loop_ap
+    jmp et_exit
+et_egale:
+    incl ap
+    loop loop_ap
+    et_exit:
+    movl $1, %eax
+    xorl %ebx, %ebx
+    int $0x80
 

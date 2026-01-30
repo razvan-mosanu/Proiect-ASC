7197b6d70e379013ba9468cc23b0da39399e33db
razvan-mosanu <razvanamosanu@gmail.com>
2026-01-30 11:50:58 +0200

    Update input.s

--- test/Lab 0x02/Ex 0xA Lab 0x02/input.s
+++ test/Lab 0x02/Ex 0xA Lab 0x02/input.s
@@ -1,1 +1,17 @@
+.data
+x: .long 77
+.text
+.global main
+main:
+    movl x, %eax
+    movl $1, %ebx
+et_loop:
+    cmpl %eax, %ebx
+    ja et_exit
+    shll $1, %ebx
+    jmp et_loop
+et_exit:
+    movl $1, %eax
+    xorl %ebx, %ebx
+    int $0x80
 

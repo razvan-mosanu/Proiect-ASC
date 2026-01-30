f6c25fe2defb6d4641e81078fd089aece582c42e
razvan-mosanu <razvanamosanu@gmail.com>
2026-01-30 11:47:12 +0200

    Update input.s

--- test/Lab 0x02/Ex 0x02 Lab 0x02/input.s
+++ test/Lab 0x02/Ex 0x02 Lab 0x02/input.s
@@ -1,1 +1,20 @@
+.data
+.text
+.global main
+main:
+    movl $0, %ecx
+    movl $3, %eax
+    movl $5, %ebx
+    cmp %ebx, %eax
+    jge greater_or_equal
+    movl $1, %ecx
+    jmp end
+
+greater_or_equal:
+    movl $2, %ecx
+
+end:
+    movl $1, %eax
+    xor %ebx, %ebx
+    int $0x80
 

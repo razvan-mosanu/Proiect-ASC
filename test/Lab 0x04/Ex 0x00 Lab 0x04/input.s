0a7133a1dde7f0bb3eb7cdf4c327f37baac8bf8a
razvan-mosanu <razvanamosanu@gmail.com>
2026-01-30 11:51:52 +0200

    Update input.s

--- test/Lab 0x04/Ex 0x00 Lab 0x04/input.s
+++ test/Lab 0x04/Ex 0x00 Lab 0x04/input.s
@@ -1,1 +1,14 @@
+.data
+n: .long 6
+v: .word 10, 20, 40, 60, 100, 5
+.text
+.global main
+main:
+    lea v, %edi
+    mov $1, %ecx
+    mov (%edi, %ecx, 4), %edx
+etexit:
+    mov $1, %eax
+    mov $0, %ebx
+    int $0x80
 

7de8fcc6478d2d2894bf60ab9f8bee92e6c89ffc
razvan-mosanu <razvanamosanu@gmail.com>
2026-01-30 11:47:35 +0200

    Update input.s

--- test/Lab 0x02/Ex 0x03 Lab 0x02/input.s
+++ test/Lab 0x02/Ex 0x03 Lab 0x02/input.s
@@ -1,1 +1,17 @@
+.data
+    x1: .ascii "maria"
+    x2: .ascii "maria1"
+    x3: .ascii "maria2"
+.text
+.global main
+main:
+    mov $4, %eax
+    mov $1, %ebx
+    mov $x1, %ecx
+    mov $14, %edx
+    int $0x80
+end:
+    movl $1, %eax
+    xor %ebx, %ebx
+    int $0x80
 
